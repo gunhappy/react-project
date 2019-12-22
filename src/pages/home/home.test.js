c739e365e39a320b09ccb3692fc78ba1f2bb11f8
gunphasin <phasin@wongnai.com>
2019-12-22 11:21:47 +0700

    setup router

--- src/pages/home/home.test.js
+++ src/pages/home/home.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from './App'
+import Home from './'
 
 test('renders learn react link', () => {
-	const { getByText } = render(<App />)
-	const linkElement = getByText(/learn react/i)
+	const { getByText } = render(<Home />)
+	const linkElement = getByText(/HomePage/i)
 	expect(linkElement).toBeInTheDocument()
 })
